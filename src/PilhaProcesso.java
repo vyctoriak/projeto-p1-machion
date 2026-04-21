27cb8d9c7865af9a675f5ec2a9f778c3cc5277ed
Vyctoria Karina <vyctoriak@gmail.com>
2026-04-21 19:45:17 -0300

    Adiciona o método peek e método para pegar o tamanho da PilhaProcesso

--- src/PilhaProcesso.java
+++ src/PilhaProcesso.java
@@ -19,6 +19,17 @@ public class PilhaProcesso {
         return dados.remover(dados.getTamanho() - 1);
     }
 
+    public Processo peek() {
+        if (estaVazio()) {
+            throw new PilhaVaziaException("Não é possível: pilha vazia!");
+        }
+        return dados.obter(dados.getTamanho() - 1);
+    }
+
+    public int tamanho() {
+        return dados.getTamanho();
+    }
+
     public boolean estaVazio() {
         return dados.estaVazio();
     }
