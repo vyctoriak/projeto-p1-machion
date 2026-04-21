099fb7a08b8da54a5ee58ce3699286d90c207d05
Vyctoria Karina <vyctoriak@gmail.com>
2026-04-21 18:13:33 -0300

    Implementa classe Processo com protocolo sequencial

---
+++ src/Processo.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Processo {
+    private static int contador = 0;
+    private final int protocolo;
+    private String solicitante;
+    private String tipoServico;
+    private int prioridade; // 1 - baixa; 2 - normal; 3 - urgente
+    private String dataHora;
+
+    public Processo(String solicitante, String tipoServico, int prioridade) {
+        contador++;
+        this.protocolo = contador;
+        this.solicitante = solicitante;
+        this.tipoServico = tipoServico;
+        this.prioridade = prioridade;
+        this.dataHora = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+}
